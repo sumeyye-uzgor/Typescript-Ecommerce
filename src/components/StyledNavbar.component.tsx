70945191d125e0dc164b4f96c373444a2b09adbb
sumeyye-uzgor <summeyyeoz@gmail.com>
2021-07-01 16:57:56 +0300

    cartDrowpdown and hover

--- src/components/StyledNavbar.component.tsx
+++ src/components/StyledNavbar.component.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap'
-import { ReactComponent as ShoppingBag } from '../assets/ShoppingBag.svg'
 import { Link } from 'react-router-dom'
+import CartIcon from './CartIcon/CartIcon.component'
 
 function StyledNavbar() {
     return (
         <Navbar expand="lg" fixed="top" bg="white" style={{ zIndex: 5 }}>
             <Container>
-                <Navbar.Brand href="/">... Clothing</Navbar.Brand>
+                <Navbar.Brand href="/">Passion Fashion</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
@@ -27,13 +27,13 @@ function StyledNavbar() {
                                 Contact
                             </Link>
                         </Nav.Link>
-
                         {/* <ShoppingBag onMouseEnter width="30px" height="40px" className="cartIcon" /> */}
                         {/* <Nav.Link as="li" > */}
                         {/* </Nav.Link> */}
                     </Nav>
                 </Navbar.Collapse>
-                <Link to="/checkout" style={{ textDecoration: "none", color: "inherit" }}><ShoppingBag width="30px" height="30px" className="cartIcon" /></Link>
+                <CartIcon />
+
 
             </Container>
         </Navbar >
